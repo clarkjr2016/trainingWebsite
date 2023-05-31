9456d10c826d39de7b7c5dfee69e18a5834305e9
Eugene Clark <clarkjr2016@gmail.com>
2023-05-30 23:47:14 -0400

    Added more to the dashboard

--- dashboard/dashboard_script.js
+++ dashboard/dashboard_script.js
@@ -5,6 +5,7 @@ const main_right = document.querySelector("#main_right");
 function toggleMenu() {
   main_left.style.display = "none";
   main_right.style.width = "100%";
+  main_right.style.gridColumn = "1 / span 2"; // this sets the right element to take up the whole width of the page after the side bar has been removed
 }
 
 menu.addEventListener("click", toggleMenu);
